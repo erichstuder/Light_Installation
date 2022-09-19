3fa5b84e8587931dfefa804d681f5ab1fbb8640b
erichstuder <richu_@hotmail.com>
2022-09-19 19:37:27 +0200

    simple led example

---
+++ software/firmware/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <WS2812FX.h>
+
+#define LED_COUNT 150
+#define LED_PIN 13
+
+WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup(){
+	ws2812fx.init();
+	ws2812fx.setBrightness(255);
+	ws2812fx.setSpeed(1000);
+	ws2812fx.setMode(FX_MODE_RAINBOW);
+	//ws2812fx.setColor(WHITE);
+	ws2812fx.start();
+}
+
+void loop(){
+	ws2812fx.service();
+}
