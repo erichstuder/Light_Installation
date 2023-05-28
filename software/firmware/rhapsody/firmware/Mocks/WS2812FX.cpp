e39892a4beea7f5b72952ee8e1033051d0215fa9
Erich Studer <e.studer@elesta-gmbh.com>
2023-05-28 19:14:02 +0200

    everything compiles

--- software/firmware/rhapsody/firmware/Mocks/WS2812FX.cpp
+++ software/firmware/rhapsody/firmware/Mocks/WS2812FX.cpp
@@ -1,3 +1,14 @@
 #include "WS2812FX.h"
 
+
+WS2812FX::WS2812FX(uint16_t num_leds, uint8_t pin, neoPixelType type,
+  uint8_t max_num_segments,
+  uint8_t max_num_active_segments){}
+WS2812FX::WS2812FX(){}
+
 void WS2812FX::service(){}
+void WS2812FX::init(){}
+void WS2812FX::setBrightness(uint8_t b){}
+void WS2812FX::setSpeed(uint16_t s){}
+void WS2812FX::setMode(uint8_t m){}
+void WS2812FX::start(){}
--- software/firmware/rhapsody/firmware/Mocks/WS2812FX.h
+++ software/firmware/rhapsody/firmware/Mocks/WS2812FX.h
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <stdint.h> //TODO: is it a good idea to include this here? for example for uint8_t
+
+//TODO: es wäre schöner diese defines direkt aus der Library zu verwenden. Oder?
+#define NEO_GRB 1
+
+#define NEO_KHZ800 2
+
+#define FX_MODE_CHASE_RAINBOW 33
+
+typedef uint16_t neoPixelType;
+
 class WS2812FX{
 public:
+	WS2812FX(uint16_t num_leds, uint8_t pin, neoPixelType type,
+      uint8_t max_num_segments,
+      uint8_t max_num_active_segments);
+	WS2812FX();
 	void service();
+	void init();
+	void setBrightness(uint8_t b);
+	void setSpeed(uint16_t s);
+	void setMode(uint8_t m);
+	void start();
 };
