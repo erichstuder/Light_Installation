c3ea9b4811ec3410adc3d67be1bb45538eb34d39
erichstuder <richu_@hotmail.com>
2023-08-05 10:30:24 +0200

    add steps template

---
+++ software/simulator/features/steps/steps.py
@@ -0,0 +1,33 @@
+@when(u'I open the simulator')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I open the simulator')
+
+
+@then(u'a model of the cube is shown')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then a model of the cube is shown')
+
+
+@then(u'the leds are shown')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the leds are shown')
+
+
+@then(u'the led animation starts rightaway')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the led animation starts rightaway')
+
+
+@given(u'the simulator is running')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the simulator is running')
+
+
+@given(u'the animation is running')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the animation is running')
+
+
+@then(u'the cube can be rotated with the mouse')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the cube can be rotated with the mouse')
