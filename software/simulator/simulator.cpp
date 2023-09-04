7462c96c2da38fcd5e49147f44ca8f8dcbae3e40
erichstuder <richu_@hotmail.com>
2023-09-04 14:02:24 +0200

    port running light into cpp

--- software/simulator/simulator.cpp
+++ software/simulator/simulator.cpp
@@ -1,48 +1,23 @@
-//#include <iostream>
-
-// Compile it with:
-//   g++ -o example-vector example-vector.cc -lboost_iostreams -lboost_system -lboost_filesystem
-
-#include <vector>
-#include <cmath>
-#include <boost/tuple/tuple.hpp>
-
-#include "gnuplot-iostream/gnuplot-iostream.h" //TODO: remove path
+#include "gnuplot-iostream.h"
 
 int main() {
 	Gnuplot gp;
+	gp	<< "set grid\n"
 
-	// Gnuplot vectors (i.e. arrows) require four columns: (x,y,dx,dy)
-	std::vector<boost::tuple<double, double, double, double> > pts_A;
+		<< "set xlabel 'x'\n"
+		<< "set ylabel 'y'\n"
+		<< "set zlabel 'z'\n"
 
-	// You can also use a separate container for each column, like so:
-	std::vector<double> pts_B_x;
-	std::vector<double> pts_B_y;
-	std::vector<double> pts_B_dx;
-	std::vector<double> pts_B_dy;
+		<< "set xrange[0:500]\n"
+		<< "set yrange[0:500]\n"
+		<< "set zrange[0:500]\n"
 
-	// Make the data
-	for(double alpha=0; alpha<1; alpha+=1.0/24.0) {
-		double theta = alpha*2.0*3.14159;
-		pts_A.push_back(boost::make_tuple(
-			 cos(theta),
-			 sin(theta),
-			-cos(theta)*0.1,
-			-sin(theta)*0.1
-		));
+		<< "rgb(x,y,z,n) = (z/33.25 == n) ? 65536*255 + 25600*255*0 + 255*0 : 0\n"
 
-		pts_B_x .push_back( cos(theta)*0.8);
-		pts_B_y .push_back( sin(theta)*0.8);
-		pts_B_dx.push_back( sin(theta)*0.1);
-		pts_B_dy.push_back(-cos(theta)*0.1);
-	}
-
-	// Don't forget to put "\n" at the end of each line!
-	gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
-	// '-' means read from stdin.  The send1d() function sends data to
-	// gnuplot's stdin.
-	gp << "plot '-' with vectors title 'pts_A', "
-		<< "'-' with vectors title 'pts_B'\n";
-	gp.send1d(pts_A);
-	gp.send1d(boost::make_tuple(pts_B_x, pts_B_y, pts_B_dx, pts_B_dy));
+		<< "n = 0\n"
+		<< "while(1){\n"
+		<< "	splot 'leds.dat' using 1:2:3:(rgb($1,$2,$3,n)) with points lt 7 lc rgbcolor variable\n"
+		<< "	n = (n + 1) % 15\n"
+		<< "	pause(0.01)\n"
+		<< "}\n";
 }
