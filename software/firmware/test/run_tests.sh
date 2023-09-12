8d9cb3c00d4e8dc3bb14d09a5a4b63ee79c82ec0
erichstuder <richu_@hotmail.com>
2023-09-12 10:43:12 +0200

    add example test

--- software/firmware/test/run_tests.sh
+++ software/firmware/test/run_tests.sh
@@ -1,4 +1,11 @@
 # make sure the tag is unique
 TAG=$(echo ${PWD:1} | tr '[:upper:]' '[:lower:]')
+WORKDIR=/home
 docker build -t $TAG .
-docker run -it $TAG
+docker run \
+	--rm \
+	--name firmware-cpputest \
+	--volume ${PWD}:${WORKDIR} \
+	--workdir ${WORKDIR} \
+	-it $TAG \
+	make
