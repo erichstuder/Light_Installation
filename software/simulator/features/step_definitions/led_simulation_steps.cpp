a5202c955a87b4f3b2d095164e19c5159fa7fe78
erichstuder <richu_@hotmail.com>
2023-09-30 15:24:38 +0200

    add dummy implementation of some steps

---
+++ software/simulator/features/step_definitions/led_simulation_steps.cpp
@@ -0,0 +1,12 @@
+THEN("^a model of the cube is shown") {}
+
+THEN("^the leds are shown") {}
+
+THEN("^the led animation starts rightaway") {}
+
+
+GIVEN("^Given the simulator is running") {}
+
+GIVEN("^the animation is running") {}
+
+THEN("^Then the cube can be rotated with the mouse") {}
