bc2a1f38699be1d835a1a4d14d1c76e26822bbea
erichstuder <richu_@hotmail.com>
2024-08-11 14:25:39 +0200

    add top level documentation for the split mbse<->textual

---
+++ doc/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import subprocess
+from sphinx.application import Sphinx
+
+project = '433MHz_to_MQTT'
+copyright = '2024, erichstuder'
+author = 'erichstuder'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    # 'sphinxcontrib.drawio',
+    # 'sphinxcontrib.plantuml',
+    # 'sphinx_toolbox.collapse',
+    'sphinxcontrib.programoutput',
+    # 'sphinxcontrib_rust',
+]
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
+
+# drawio_no_sandbox = True
---
+++ doc/source/index.rst
@@ -0,0 +1,44 @@
+Welcome to Light Installation's documentation!
+==============================================
+
+Note: The documentation has no direct links to files in the project.
+This is because there shall be no broken links, which is not possible if the links shall work locally and relative to the online repository.
+Relevant parts of files and generated documentation is of course part of the documentation.
+
+This project is developed in parallel as an MBSE (Model Based System Engineering) project and a textual project.
+The is done to compare the two approaches.
+The folder MBSE contains the MBSE project and the folder Textual contains the textual project.
+The folder Shared contains files that are shared between the two projects.
+See :doc:`toplevel_file_structure`.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   toplevel_file_structure
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+References
+==========
+
+Repository
+
+.. program-output:: git remote get-url origin
+
+Branch:
+
+.. program-output:: git rev-parse --abbrev-ref HEAD
+
+Hash:
+
+.. program-output:: git rev-parse HEAD
+
+Creation Date:
+
+   |today|
