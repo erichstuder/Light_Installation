192162ac09c3b2e60179592865744979236426a3
erichstuder <richu_@hotmail.com>
2024-08-23 15:23:34 +0200

    add generation of constructors

--- MBSE/workspace/model/code/LedStrip.h
+++ MBSE/workspace/model/code/LedStrip.h
@@ -5,4 +5,7 @@
 class LedStrip {
     public:
         ~LedStrip() {}
+        void LedStrip(EByte pin, AnimationCallback_t animationCallback, EInt numberOfLeds);
+        void setPixelColor(EByte z, EByte r, EByte g, EByte b);
+        void animate();
 };
--- MBSE/workspace/model/code/LedStrip_Factory.cpp
+++ MBSE/workspace/model/code/LedStrip_Factory.cpp
@@ -2,9 +2,10 @@
 #include "LedStrip_Factory_Interface.h"
 
 LedStrip_Interface LedStrip_Factory::create(EByte pin, AnimationCallback_t animationCallback, EInt numberOfLeds) {
-    // Start of user code (don't remove the 'Start/End of user code' markers)
+    // Start of user code create (don't remove the 'Start/End of user code' markers)
 
-	return new LedStrip(pin, animationCallback, numberOfLeds);
+    return new LedStrip(pin, animationCallback, numberOfLeds);
 
     // End of user code
 }
+
