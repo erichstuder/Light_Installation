3e4858c7163bdd8213adb267a03927d39d6ce019
erichstuder <richu_@hotmail.com>
2024-09-05 18:23:22 +0200

    bugfixes in creation of production data

--- Shared/electronics/create_production_data.sh
+++ Shared/electronics/create_production_data.sh
@@ -1,22 +1,13 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo $SCRIPT_DIR
-echo $(pwd)
+docker run --rm --volume $(pwd)/../..:/home/project --workdir /home/project/Shared/electronics/cross_connector kibot_container \
+    '-c cross_connector.kibot.yaml' \
+    '-b cross_connector.kicad_pcb' \
+    '-e cross_connector.kicad_sch' \
+	'-d production_data'
 
+docker run --rm --volume $(pwd)/../..:/home/project --workdir /home/project/Shared/electronics/led_connector kibot_container \
+    '-c led_connector.kibot.yaml' \
+    '-b led_connector.kicad_pcb' \
+    '-e led_connector.kicad_sch' \
+	'-d production_data'
 
-
-mkdir -p temp/production_data
-rm -rf temp/production_data/*
-
-docker run --rm --volume $(pwd)/../..:/home/project --workdir /home/project kibot_container \
-    '-c Shared/electronics/cross_connector/cross_connector.kibot.yaml' \
-    '-b Shared/electronics/cross_connector/cross_connector.kicad_pcb' \
-    '-e Shared/electronics/cross_connector/cross_connector.kicad_sch'
-
-
-exit 0
-
-docker run --rm --volume ${SCRIPT_DIR}/../..:/home/project --workdir /home kibot_container \
-    '-c project/mechanics/assembly/assembly.kibot.yaml' \
-    '-b project/electronics/cross_connector/cross_connector.kicad_pcb'
-
-openscad -o ${SCRIPT_DIR}/temp/3d_models/clamp.stl ../clamp/clamp.scad
+# openscad -o ${SCRIPT_DIR}/temp/3d_models/clamp.stl ../clamp/clamp.scad
