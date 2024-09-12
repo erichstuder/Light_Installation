39ce6cf1bf32eb6aa9c37dec2651599f45b13ceb
erichstuder <richu_@hotmail.com>
2024-09-12 18:34:59 +0200

    make cucumber tests compile

--- Textual/simulator/features/run_cucumber.sh
+++ Textual/simulator/features/run_cucumber.sh
@@ -4,11 +4,11 @@
 TAG=$(echo ${PWD:1} | tr '[:upper:]' '[:lower:]')
 WORKDIR=/usr/software
 echo "building container, please wait (the first time this may take several minutes) ..."
-docker build -t $TAG . >/dev/nul
+docker build -t $TAG .
 docker run \
 	--rm \
 	--name cucumber_$(date +%s) \
-	--volume "${PWD}/../..":${WORKDIR} \
-	--workdir ${WORKDIR}/simulator/features \
+	--volume "${PWD}/../../..":${WORKDIR} \
+	--workdir ${WORKDIR}/Textual/simulator/features \
 	-it $TAG \
 	bash -c "cmake . && cmake --build . && (./simulator_steps >/dev/null &) && cucumber ."
